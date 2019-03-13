a5185fa750be72934c260455803925192765f24f
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-13 14:11:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,8 @@ class Shoe
     @brand = brand 
   end 
   
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :condition
   
   def cobble
     puts "Your shoe is as good as new!"
