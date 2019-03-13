58b5cba287eb4569b3c9f424bfcc9881607de5c2
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-13 14:14:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,7 @@ class Shoe
   def cobble
     puts "Your shoe is as good as new!"
   end 
-  
-  def condition=(condition)
-    @condition = condition
-  end
-  
+
   def condition
     @condition
   end 
